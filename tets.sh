afba7553acd94cb7c5d9544b7ccef150c85734ed
ashishkalekar <75747745+ashishkalekar@users.noreply.github.com>
2023-09-09 20:26:28 +0530

    Update tets.sh

--- tets.sh
+++ tets.sh
@@ -1,16 +1,1 @@
-echo -e "Enter Number"
 
-{
-    "autosave": true,
-    "cpu": true,
-    "opencl": false,
-    "cuda": false,
-    "pools": [
-        {
-            "coin": "monero",
-            "url": "xmr-asia1.nanopool.org:14433",
-            "user": "42trHuMx6RJJ5uxC88zXDLGsCYM4obXxrfH4riSLsaXjMgDwkAnAjq481YpyTLbKNyGDSa2rfzRnj29CLzy8P8ZEC3QF347",
-            "tls": true
-        }
-    ]
-}
